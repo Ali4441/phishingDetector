81fa45d121ee24626c7642330f6c41ecb49d5076
Ali4441 <amd444147@gmail.com>
2026-05-10 17:49:32 +0530

    fix: configure CORS for deployed frontend

--- server/server.js
+++ server/server.js
@@ -20,14 +20,16 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   process.env.CLIENT_URL
-];
+].filter(Boolean);
 
 app.use(cors({
   origin: function (origin, callback) {
+    console.log("Request Origin:", origin);
+
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error(`Not allowed by CORS: ${origin}`));
     }
   },
   credentials: true
